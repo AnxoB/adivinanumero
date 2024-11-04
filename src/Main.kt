7908bdb050adbe189ceccbb212d0df84f3faa638
Anxo Bua Alonso <a22anxoba@iessanclemente.net>
2024-11-04 20:00:40 +0100

    Versión 0.2

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,7 @@ import kotlin.random.Random
 const val BG_RED = "\u001B[41m"
 const val BG_GREEN = "\u001B[42m"
 const val RESET = "\u001B[0m"
+const val BG_YELLOW = "\u001B[43m"
 
 fun main(){
 
@@ -20,15 +21,21 @@ fun main(){
 
 }
 
-fun jugar(){
-    var numeroAleatorio = numeroAleatorio()
-    for (j in 0 until 10){
-        print("Teclea un numero de 4 cifras sin números repetidos: ")
-        var numero= readln()
-        compararNumeros(numeroAleatorio,numero)
+fun jugar() {
+    val numeroAleatorio = numeroAleatorio()
+    for (j in 0 until 10) {
+        var numero: String
+        do {
+            print("Teclea un número de 4 cifras sin números repetidos: ")
+            numero = readln()
+            if (numero.length != 4 || !esUnico(numero)) {
+                println("Error: El número debe tener 4 cifras y no contener dígitos repetidos.")
+            }
+        } while (numero.length != 4 || !esUnico(numero))
 
+        compararNumeros(numeroAleatorio, numero)
     }
-
+    println("Lo siento, no has logrado acertar el número en 10 intentos. El número era: $numeroAleatorio")
 }
 
 fun historial(){
@@ -41,21 +48,32 @@ fun numeroAleatorio(): String {
     return digitos.take(4).joinToString("")
 }
 
-fun compararNumeros(numeroAleatorio: String, numero: String){
+fun compararNumeros(numeroAleatorio: String, numero: String):Boolean{
     var acierto = 0
+    var coincidencia = 0
     var fallo = 0
     for (i in 0 until 4) {
         if (numero[i] == numeroAleatorio[i]) {
             acierto++
             if (acierto == 4) {
+                println(numero +  "    ${BG_GREEN}  $acierto  ${BG_YELLOW}  $coincidencia  ${BG_RED}  $fallo  ${RESET}")
                 println("Felicidades, has acertado el número!")
-                return
+                return true
             }
         } else {
-            fallo++
+            if (numeroAleatorio.contains(numero[i])){
+                coincidencia++
+            } else{
+                fallo++
+            }
         }
     }
-    println(numero + " " + numeroAleatorio +  "    ${BG_GREEN}  $acierto  ${BG_RED}  $fallo  ${RESET}")
+    println(numero + "    ${BG_GREEN}  $acierto  ${BG_YELLOW}  $coincidencia  ${BG_RED}  $fallo  ${RESET}")
+    return false
+}
+
+fun esUnico(numero: String): Boolean {
+    return numero.toSet().size == numero.length
 }
 
 
